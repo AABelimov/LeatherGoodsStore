8922826d5a13188339cc2984af6d1f71eb9690c1
AABelimov <belimov98@gmail.com>
2024-07-02 13:28:22 +0300

    initial commit

---
+++ src/main/java/ru/aabelimov/leathergoodsstore/LeatherGoodsStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.aabelimov.leathergoodsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeatherGoodsStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeatherGoodsStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/aabelimov/leathergoodsstore/LeatherGoodsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aabelimov.leathergoodsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeatherGoodsStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
