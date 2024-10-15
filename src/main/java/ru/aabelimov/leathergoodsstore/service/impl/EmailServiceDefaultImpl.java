b4d18d0c4f49da5ba5e51537aa398f0d78fd008c
AABelimov <belimov98@gmail.com>
2024-10-15 13:17:07 +0300

    added images in email notification about order. sending message in a separate thread

--- src/main/java/ru/aabelimov/leathergoodsstore/service/impl/EmailServiceDefaultImpl.java
+++ src/main/java/ru/aabelimov/leathergoodsstore/service/impl/EmailServiceDefaultImpl.java
@@ -4,13 +4,21 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import ru.aabelimov.leathergoodsstore.entity.Image;
 import ru.aabelimov.leathergoodsstore.service.EmailService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -33,14 +41,24 @@ public class EmailServiceDefaultImpl implements EmailService {
     }
 
     @Override
-    public void sendHtmlMessage(String to, String subject, String template, Context context) throws MessagingException {
+    public void sendHtmlMessage(String to, String subject, String template, Context context, List<Image> productsImages) throws MessagingException {
         MimeMessage mimeMessage = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
         String htmlContent = templateEngine.process(template, context);
 
+        helper.setFrom(mailUsername);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent, true);
+
+        productsImages.forEach(image -> {
+            try {
+                InputStreamSource imageSource = new ByteArrayResource(Files.readAllBytes(Path.of(image.getImagePath())));
+                helper.addInline(image.getImagePath(), imageSource, image.getMediaType());
+            } catch (IOException | MessagingException e) {
+                throw new RuntimeException(e); // TODO ::
+            }
+        });
 
         emailSender.send(mimeMessage);
     }
